3c133e8e221fff2737ae3010e2cf98b628791085
JohnTian <tianzhaixing@126.com>
2015-12-10 16:06:36 +0800

    add shaishufang.

---
+++ Crawl/Shaishufang/getBookdataByISBN.py
@@ -0,0 +1,59 @@
+# -*- coding:utf-8 -*-
+import unirest
+import json
+from collections import OrderedDict
+import time
+from multiprocessing import Pool
+import socket
+
+def PutData(isbnurls):
+    """ data """
+    spidername = 'ShaishufangAmazon'
+    cnt = 0
+    for url in isbnurls:
+	cnt += 1
+	unirest.timeout(180)
+        response = unirest.get(url, headers={"Accept":"application/json"}) # handle url = baseurl + isbn
+        try:
+            #bookdt
+            bookdt = response.body['isbn']
+            bookdt['spider'] = spidername
+	    #data style
+	    datadict = {}
+            datadict['datas'] = [bookdt]
+
+            #put datadict
+            unirest.timeout(180)
+            resdata = unirest.put(
+                            "http://192.168.100.3:5000/data",
+                            headers={ "Accept": "application/json", "Content-Type": "application/json" },
+                            params=json.dumps(datadict)
+                         )
+        except:
+            pass
+	if ((cnt%300)==0):
+	    time.sleep(3)	
+
+if __name__ == '__main__':
+
+    baseurl = 'http://192.168.100.3:5001/book?isbn='
+    isbnurls = []
+    with file('./shaishufang.isbns.txt', 'rb') as fi:
+         for line in fi.readlines():
+             isbnurls.append(baseurl + line.strip())
+    fi.close()
+    #print len(isbnurls), isbnurls[0:10]
+    	
+    PutData(isbnurls[49286:])
+
+    """
+    testurl = isbnurls[:10]
+    
+    t1 = time.time()
+    pool = Pool()
+    pool.map(PutData, testurl)
+    pool.close()
+    pool.join()
+    t2 = time.time()
+    print (t1-t2)
+    """
